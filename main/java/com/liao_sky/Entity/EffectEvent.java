197bac7ba589b0e492d8878c25482a8b1e606412
liao-sky <78252268+liao-sky@users.noreply.github.com>
2025-01-29 19:00:37 +0800

    1

--- main/java/com/liao_sky/Entity/EffectEvent.java
+++ main/java/com/liao_sky/Entity/EffectEvent.java
@@ -8,6 +8,8 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.text.*;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.world.BlockEvent;
@@ -16,22 +18,20 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = Sky.MOD_ID)
 public class EffectEvent {
-
     @Cancelable
     public static class ErosionEvent{
-
         public static float P = 1.0f;
-
         public static boolean Erosion(LivingHurtEvent event) {
             if (event.isCanceled()) {
                 return false;
             }
             LivingEntity beHurt = event.getEntityLiving();//受伤对象
             DamageSource source = event.getSource();
+            World world = beHurt.getCommandSenderWorld();
+            Difficulty hard = world.getDifficulty();
             float amount = event.getAmount();
             float armorValue = beHurt.getArmorValue();
             float maxHealth = beHurt.getMaxHealth();
-            float difference = amount - armorValue;
             int amplified = 0;
             if (amount > 0.25 * (armorValue + 0.1 * maxHealth) + 0.5) {
                 amplified = 1;
@@ -42,7 +42,7 @@ public class EffectEvent {
             } else if (amount > armorValue + 0.2 * maxHealth + 0.5) {
                 amplified = 4;
             }
-            if (Probability.P(0.25f * P * amplified) && difference > 0 && source == DamageSource.GENERIC) {
+            if (Probability.P(0.25f * Probability.DtoP(hard)* P * ++amplified) && source == DamageSource.GENERIC) {
                 beHurt.addEffect(new EffectInstance(RegistryEvents.EffectRegistry.erosion.get(), (int) (20 * amount), amplified));
                 beHurt.sendMessage(new StringTextComponent("§d甲爆哩？！§d"+ (amplified + 1)),beHurt.getUUID());
             }
@@ -75,18 +75,20 @@ public class EffectEvent {
                 return false;
             }
             PlayerEntity Player = event.getPlayer();
+            World world = Player.getCommandSenderWorld();
+            Difficulty hard = world.getDifficulty();
             EffectInstance Effect = Player.getEffect(RegistryEvents.EffectRegistry.fracture.get());
             if (Effect != null) {
                 int amplified = Effect.getAmplifier()+1;
                 if (Probability.P(0.5f)){
-                    Player.hurt(new DamageSource("fracture").bypassArmor().bypassInvul(), Damage * amplified);
+                    Player.hurt(new DamageSource("fracture").bypassArmor().bypassInvul(), Damage * amplified * Probability.DtoP(hard));
                     Player.sendMessage(new StringTextComponent("§d哎哟！受伤了§d"),Player.getUUID());
                 }
                 if (Probability.P(0.25f)) {
                     Player.addEffect(new EffectInstance(RegistryEvents.EffectRegistry.fracture.get(), 20 * 10 * amplified, amplified));
                     Player.sendMessage(new StringTextComponent("§d杂鱼~骨折严重了~§d"),Player.getUUID());
                 }
-            }else if (Probability.P(0.05f * P)) {
+            }else if (Probability.P(0.05f * P * Probability.DtoP(hard))) {
                 Player.addEffect(new EffectInstance(RegistryEvents.EffectRegistry.fracture.get(), 20 * 10, 0));
                 Player.sendMessage(new StringTextComponent("§d骨折了§d"),Player.getUUID());
             }
--- main/java/com/liao_sky/Random/Probability.java
+++ main/java/com/liao_sky/Random/Probability.java
@@ -1,9 +1,28 @@
 package com.liao_sky.Random;
 
+import net.minecraft.world.Difficulty;
+
+import java.util.Random;
+
+
 public class Probability {
     public static boolean P(float P){
-        java.util.Random random = new java.util.Random();
+        Random random = new Random();
         int r = random.nextInt(101);
         return r < P * 100;
+    }
+
+    public static float DtoP(Difficulty D){
+        float P = 0.0f;
+        if(D==Difficulty.PEACEFUL){
+                P =0.0f;
+            } else if (D==Difficulty.EASY) {
+                P =1.0f;
+            } else if (D==Difficulty.NORMAL) {
+                P =1.2f;
+            } else if (D==Difficulty.HARD) {
+                P =1.5f;
+        }
+        return P;
     }
 }
