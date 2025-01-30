471c241b859622b5b81aee97534f1815677e8b61
liao-sky <78252268+liao-sky@users.noreply.github.com>
2025-01-30 23:35:11 +0800

    1

--- main/java/com/liao_sky/Entity/EffectEvent.java
+++ main/java/com/liao_sky/Entity/EffectEvent.java
@@ -3,8 +3,13 @@ package com.liao_sky.Entity;
 import com.liao_sky.Events.RegistryEvents;
 import com.liao_sky.Random.Probability;
 import com.liao_sky.Sky;
+import net.minecraft.block.Block;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ToolItem;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.text.*;
@@ -29,10 +34,11 @@ public class EffectEvent {
             DamageSource source = event.getSource();
             World world = beHurt.getCommandSenderWorld();
             Difficulty hard = world.getDifficulty();
+            Iterable<ItemStack> Slots =beHurt.getArmorSlots();
             float amount = event.getAmount();
             int ArmorValue = beHurt.getArmorValue();
             EffectInstance Effect = beHurt.getEffect(RegistryEvents.EffectRegistry.erosion.get());
-            if(Effect!=null && amount>ArmorValue*0.2f){
+            if(Effect!=null && amount>ArmorValue*0.3f){
                 int amplified = Effect.getAmplifier();
                 if (Probability.P(0.25f * Probability.DtoP(hard)* P)){
                     event.setAmount(amount*((amplified+1)*Probability.DtoP(hard)+10)/10);
@@ -42,6 +48,15 @@ public class EffectEvent {
                     amplified++;
                     beHurt.addEffect(new EffectInstance(RegistryEvents.EffectRegistry.erosion.get(), (int) (20 * amount * Probability.DtoP(hard)), amplified));
                     beHurt.sendMessage(new StringTextComponent("§d甲又爆哩？！+1§d §4受到伤害x"+(10+amplified*Probability.DtoP(hard))/10+"(有概率)§4"),beHurt.getUUID());
+                }
+                if (Probability.P(0.05f*P) && amplified >= 10){
+                    for (ItemStack slot : Slots) {
+                        if(slot.getStack().getEquipmentSlot()!= EquipmentSlotType.MAINHAND && Probability.P(0.25f*P)){
+                            slot.setCount(0);
+                            beHurt.sendMessage(new StringTextComponent("§4！你的甲不堪重负！§4"),beHurt.getUUID());
+                            //slot.hurtAndBreak();
+                        }
+                    }
                 }
             }
             else if (Probability.P(0.25f * Probability.DtoP(hard)* P) && source.getDirectEntity()!=null && amount>ArmorValue*0.2f) {
@@ -77,10 +92,13 @@ public class EffectEvent {
                 return false;
             }
             PlayerEntity Player = event.getPlayer();
+            Item Tool = Player.getItemBySlot(EquipmentSlotType.MAINHAND).getItem();
+            Block Target = event.getState().getBlock();
             World world = Player.getCommandSenderWorld();
             Difficulty hard = world.getDifficulty();
             EffectInstance Effect = Player.getEffect(RegistryEvents.EffectRegistry.fracture.get());
-            if (Effect != null) {
+            boolean Flag = Tool.isCorrectToolForDrops(Target.defaultBlockState());
+            if (Effect != null && Flag) {
                 int amplified = Effect.getAmplifier()+1;
                 if (Probability.P(0.5f*Probability.DtoP(hard))){
                     Player.hurt(new DamageSource("fracture").bypassArmor().bypassInvul(), Damage * amplified * Probability.DtoP(hard));
@@ -90,7 +108,7 @@ public class EffectEvent {
                     Player.addEffect(new EffectInstance(RegistryEvents.EffectRegistry.fracture.get(), (int) (20 * 10 * amplified * Probability.DtoP(hard)), amplified));
                     Player.sendMessage(new StringTextComponent("§d杂鱼~骨折严重了~§d"),Player.getUUID());
                 }
-            }else if (Probability.P(0.05f * P * Probability.DtoP(hard))) {
+            }else if (Probability.P(0.05f * P * Probability.DtoP(hard)) && Flag) {
                 Player.addEffect(new EffectInstance(RegistryEvents.EffectRegistry.fracture.get(), (int) (20 * 10 * Probability.DtoP(hard)), 0));
                 Player.sendMessage(new StringTextComponent("§d骨折了§d"),Player.getUUID());
             }
